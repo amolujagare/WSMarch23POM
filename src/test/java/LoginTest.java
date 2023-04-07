3c2b87cd444cd1aa00a42c899e38f5e1b7443f69
amolu <amolujagare@gmail.com>
2023-04-07 08:36:25 +0530

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest()
+    {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost/stock/");
+
+        Login login = new Login(driver);
+        login.setTxtUsername("admin");
+        login.setTxtPassword("admin");
+        login.clickLogin();
+    }
+}
